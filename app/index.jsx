ac1ee0b9e35c844e03b4b8fcd4d0d8c6437a5615
MichalC <m.colautti@o2.pl>
2025-10-20 16:21:12 +0200

    Added header and search bar

--- app/index.jsx
+++ app/index.jsx
@@ -5,9 +5,12 @@ import {
   ActivityIndicator,
   StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
+import AddChatIcon from "../assets/icons/addChat.svg";
+import SearchIcon from "../assets/icons/search.svg";
 
 const HomeScreen = () => {
   const { user, loading, logout } = useAuth();
@@ -21,22 +24,29 @@ const HomeScreen = () => {
   }, [user, loading, router]);
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       {loading ? (
         <ActivityIndicator style={{ flex: 1 }} />
       ) : user ? (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text>Welcome: {user.username || user.email}</Text>
+        <View style={styles.chatScreen}>
+          {/* header */}
+          <View style={styles.header}>
+            <View style={styles.profileImg} />
+            <Text style={styles.title}>Cipher</Text>
+            <TouchableOpacity style={styles.addButton}>
+              <AddChatIcon width={40} height={40} stroke={"#fff"} />
+            </TouchableOpacity>
+          </View>
 
-          <TouchableOpacity style={styles.logoutButton} onPress={logout}>
-            <Text style={styles.logoutButtonText}>Logout</Text>
-          </TouchableOpacity>
+          {/* search bar */}
+          <View style={styles.searchContainer}>
+            <SearchIcon width={20} height={20} color={"#fff"} style={styles.searchIcon} />
+            <TextInput
+              placeholder="Search" 
+              placeholderTextColor={"#F9F6F0"}
+              style={styles.searchBar}
+            />
+          </View>
         </View>
       ) : null}
     </View>
@@ -44,15 +54,50 @@ const HomeScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  logoutButton: {
-    backgroundColor: "red",
-    borderRadius: 8,
-    padding: 10,
-    marginTop: 10,
+  container: {
+    flex: 1,
+    backgroundColor: "#212427",
+    padding: 20,
   },
-  logoutButtonText: {
+  chatScreen: {
+    marginHorizontal: 10,
+  },
+  header: {
+    alignItems: "center",
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    flex: 1,
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#FFFFFF",
     textAlign: "center",
+  },
+  profileImg: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#007bff",
+  },
+  addButton: {
     color: "#fff",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#383D42",
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 16,
+  },
+  searchBar: {
+    marginLeft: 10,
+    opacity: 0.5,
+  },
+  searchIcon: {
+    opacity: 0.5,
   },
 });
 
